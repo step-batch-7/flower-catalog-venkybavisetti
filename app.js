724ab9c5de61e7b0f984f15642d955640b616262
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-01-26 16:28:17 +0530

    refactoried code

--- app.js
+++ app.js
@@ -2,54 +2,31 @@ const fs = require("fs");
 const Response = require("./lib/response");
 const { loadTemplate } = require("./lib/viewTemplate");
 const CONTENT_TYPES = require("./lib/mimeTypes");
-const STATIC_FOLDER = `${__dirname}/public`;
+
+const getPath = function(url, extension) {
+  const STATIC_FOLDER = `${__dirname}/public`;
+  const htmlFile = extension === "html" ? "/html" : "";
+  return `${STATIC_FOLDER}${htmlFile}${url}`;
+};
 
 const serveStaticFile = req => {
-  const path = `${STATIC_FOLDER}${req.url}`;
-  const stat = fs.existsSync(path) && fs.statSync(path);
-  if (!stat || !stat.isFile()) return new Response();
-  const [, extension] = path.match(/.*\.(.*)$/) || [];
-  const contentType = CONTENT_TYPES[extension];
+  const url = req.url === "/" ? "/index.html" : req.url;
+  const [, extension] = url.match(/.*\.(.*)$/) || [];
+  const path = getPath(url, extension);
+  const isFileExits = fs.existsSync(path) && fs.statSync(path).isFile();
+  if (!isFileExits) return new Response();
   const content = fs.readFileSync(path);
   const res = new Response();
-  res.setHeader("Content-Type", contentType);
+  res.setHeader("Content-Type", CONTENT_TYPES[extension]);
   res.setHeader("Content-Length", content.length);
   res.statusCode = 200;
   res.body = content;
   return res;
 };
 
-const serveHomePage = req => {
-  const html = loadTemplate("index.html", {});
-  const res = new Response();
-  res.setHeader("Content-Type", CONTENT_TYPES.html);
-  res.setHeader("Content-Length", html.length);
-  res.statusCode = 200;
-  res.body = html;
-  return res;
-};
-const abeliophyllumPage = function(req) {
-  const html = loadTemplate("Abeliophyllum.html", {});
-  const res = new Response();
-  res.setHeader("Content-Type", CONTENT_TYPES.html);
-  res.setHeader("Content-Length", html.length);
-  res.statusCode = 200;
-  res.body = html;
-  return res;
-};
-
-const ageratumPage = function(req) {
-  const html = loadTemplate("Ageratum.html", {});
-  const res = new Response();
-  res.setHeader("Content-Type", CONTENT_TYPES.html);
-  res.setHeader("Content-Length", html.length);
-  res.statusCode = 200;
-  res.body = html;
-  return res;
-};
-
 const guestBookPage = function(req) {
-  const html = loadTemplate("guestBook.html", {});
+  const comments = loadComments();
+  const html = loadTemplate("guestBook.html", getTableRows(comments));
   const res = new Response();
   res.setHeader("Content-Type", CONTENT_TYPES.html);
   res.setHeader("Content-Length", html.length);
@@ -59,9 +36,10 @@ const guestBookPage = function(req) {
 };
 
 const loadComments = function() {
-  const content = fs.readFileSync("./data/comments.json", "utf8");
-  if (content) return JSON.parse(content);
-  return [];
+  const path = "./data/comments.json";
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) return [];
+  return require("./data/comments.json");
 };
 
 const storeComments = function(comments) {
@@ -69,7 +47,7 @@ const storeComments = function(comments) {
   fs.writeFileSync("./data/comments.json", string, "utf8");
 };
 
-const loadHtmlOnComments = function(comments) {
+const getTableRows = function(comments) {
   console.log(comments);
   const commentsTable = comments
     .map(comment => {
@@ -86,11 +64,11 @@ const loadHtmlOnComments = function(comments) {
 
 const onComment = function(req) {
   let comments = loadComments();
-  let newComment = req.body;
-  newComment.date = new Date().toGMTString();
-  comments.push(newComment);
+  let { name, comment } = req.body;
+  let date = new Date().toGMTString();
+  comments.unshift({ name, comment, date });
   storeComments(comments);
-  const html = loadTemplate("guestBook.html", loadHtmlOnComments(comments));
+  const html = loadTemplate("guestBook.html", getTableRows(comments));
   const res = new Response();
   res.setHeader("Content-Type", CONTENT_TYPES.html);
   res.setHeader("Content-Length", html.length);
@@ -99,21 +77,23 @@ const onComment = function(req) {
   return res;
 };
 
-const findHandler = req => {
-  if (req.method === "GET" && (req.url === "/" || req.url === "/index.html"))
-    return serveHomePage;
-  if (req.method === "GET" && req.url === "/Abeliophyllum.html")
-    return abeliophyllumPage;
-  if (req.method === "GET" && req.url === "/Ageratum.html") return ageratumPage;
-  if (req.method === "GET" && req.url === "/guestBook.html")
-    return guestBookPage;
-  if (req.method === "POST" && req.url === "/submitComment") return onComment;
-  if (req.method === "GET") return serveStaticFile;
-  return () => new Response();
+const getHandlers = {
+  "/guestBook.html": guestBookPage,
+  forAll: serveStaticFile
+};
+
+const postHandlers = {
+  "/submitComment": onComment
+};
+
+const handlers = {
+  GET: getHandlers,
+  POST: postHandlers
 };
 
 const processRequest = function(req) {
-  const handler = findHandler(req);
+  const handlerType = handlers[req.method];
+  const handler = handlerType[req.url] || handlerType.forAll || new Response();
   return handler(req);
 };
 
