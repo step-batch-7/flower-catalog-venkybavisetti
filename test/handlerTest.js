c7f00d5a953f013bb7923f2272a09788fa6d7e01
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-01-31 19:05:51 +0530

    added test cases to the home page

--- test/handlerTest.js
+++ test/handlerTest.js
@@ -1,5 +1,9 @@
+'use strict';
 const request = require('supertest');
+const fs = require('fs');
+const sinon = require('sinon');
 const { app } = require('../handlers.js');
+const App = app.serve.bind(app);
 
 describe('GET Home Page', () => {
   it('should get the home page / path', done => {
@@ -7,15 +11,48 @@ describe('GET Home Page', () => {
       .get('/')
       .set('Accept', '*/*')
       .expect(200)
+      .expect('Content-Length', '777')
       .expect('Content-Type', 'text/html', done);
+  });
+  it('should get the path /css/homeStyles.css', done => {
+    request(App)
+      .get('/css/homeStyles.css')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/css')
+      .expect('Content-Length', '229', done);
+  });
+  it('should get the path /js/hideImage.js', done => {
+    request(App)
+      .get('/js/index.js')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'application/javascript')
+      .expect('Content-Length', '298', done);
+  });
+  it('should get the path /images/freshorigins.jpg', done => {
+    request(App)
+      .get('/images/freshorigins.jpg')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'image/jpg', done);
+  });
+  it('should get the path /images/animated-flower-image-0021.gif', done => {
+    request(App)
+      .get('/images/animated-flower-image-0021.gif')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'image/gif', done);
   });
 });
 
 describe('POST /submitComment', () => {
+  beforeEach(() => sinon.replace(fs, 'writeFileSync', () => {}));
+  afterEach(() => sinon.restore());
   it('should post on the submitComment url', done => {
     request(app.serve.bind(app))
       .post('/submitComment')
-      .send('name=Ranbir&comment=hi+venky')
+      .send('name=naveen&comment=hello')
       .expect(301)
       .expect('Location', '/guestBook.html', done);
   });
